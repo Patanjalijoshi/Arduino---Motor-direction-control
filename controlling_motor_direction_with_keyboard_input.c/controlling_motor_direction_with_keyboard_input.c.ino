2156a2f3df0a980f274b81898808a268544c481a
Patanjali <joshipattu12@gmail.com>
2022-08-30 19:05:41 +0530

    Update controlling_motor_direction_with_keyboard_input.c.ino

--- controlling_motor_direction_with_keyboard_input.c/controlling_motor_direction_with_keyboard_input.c.ino
+++ controlling_motor_direction_with_keyboard_input.c/controlling_motor_direction_with_keyboard_input.c.ino
@@ -1,28 +1,30 @@
 // controlling motor direction with keyboard input
-
 char inByte;
-#define motorPin1 2
-#define motorPin2 3
+#define motorPin1 5
+#define motorPin2 6
 void setup() {
 pinMode(motorPin1,OUTPUT);
 pinMode(motorPin2,OUTPUT);
-
+Serial.begin(57600);
 }
 
 void loop() {
-  if(Serial.available()> 0){
-    if(inByte == '1'){
+  
+    inByte = Serial.read();
+    if(inByte == 'c'){
      digitalWrite(motorPin1,HIGH);
      digitalWrite(motorPin2,LOW);
+     //Serial.print("1");
       }
-    else if(inByte == '0'){
+   else if(inByte == 'a'){
      digitalWrite(motorPin1,LOW);
      digitalWrite(motorPin2,HIGH);
+      //Serial.print("0");
       }
     else{
      digitalWrite(motorPin1,LOW);
      digitalWrite(motorPin2,LOW);
       } 
-   }
+   
 
 }
